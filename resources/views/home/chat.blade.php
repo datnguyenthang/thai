83250a3628a19ec99e398d66dfe83d5a1d025c7e
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-06-13 22:16:23 -0400

    fix

--- resources/views/home/chat.blade.php
+++ resources/views/home/chat.blade.php
@@ -1,6 +1,6 @@
 <div class="chaty"></div>
 
-<script type="text/javascript" id="chaty-front-end-js-extra">
+<script type="module" id="chaty-front-end-js-extra">
     var chaty_settings = {
         "ajax_url": "https:\/\/seudamgo.com\/wp-admin\/admin-ajax.php",
         "analytics": "0",
