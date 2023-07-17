fa44691433dd4357c873b11a60e69364421c85b9
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-07-16 22:18:04 -0400

    update

---
+++ config/lg.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    // routes configurations
+    'builder_prefix' => 'cms', // prefix for builder
+
+    'middleware' => 'user-access:manager', // middleware for builder
+
+    'frontend_prefix' => '', // prefix for frontend
+
+    /* Define additional translation languages. */
+    'languages' => [],
+
+];
