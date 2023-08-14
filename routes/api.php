9adc36bbfef46578edc55c45e3e605fd6025c65f
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-08-14 01:40:33 -0400

    update

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //handle event from Omise
-Route::post('/webhook', [OmiseWebhookController::class, 'handleWebhook']);
+Route::post('/omise-webhook', [OmiseWebhookController::class, 'handleWebhook']);
