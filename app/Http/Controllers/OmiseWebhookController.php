547c347fab0735a3184d5795fdbe48d542a6e6d6
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-08-14 03:08:09 -0400

    update

--- app/Http/Controllers/OmiseWebhookController.php
+++ app/Http/Controllers/OmiseWebhookController.php
@@ -8,16 +8,14 @@ use Livewire\Component;
 class OmiseWebhookController extends Component
 {
     public function handleWebhook(Request $request) {
-        if (!$request->data) {
-            return response()->json(['message' => 'Event not found.'], 404);
-        }
+        //if (!$request->data) {
+        //    return response()->json(['message' => 'Event not found.'], 404);
+        //}
 
         $eventData = $request->data;
 
         // Emit a Livewire event to notify the component
         
         $this->emit('webhookEventReceived', $eventData);
-
-        return response()->json(['message' => 'Webhook event received.']);
     }
 }
