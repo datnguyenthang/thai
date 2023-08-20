7eb6d37ad1aee3092f3305dcd33602e7c2cda8ec
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-08-19 20:39:15 -0400

    update spinner

--- resources/views/livewire/frontend/homepage/payment/loading.blade.php
+++ resources/views/livewire/frontend/homepage/payment/loading.blade.php
@@ -1,6 +1,8 @@
 <!-- Loading Overlay, show loading to lock user action-->
 <div id="overlay" wire:loading>
-    <div class="spinner-grow text-danger"></div>
+    <div class="spinner-grow text-danger" style="width: 3rem; height: 3rem;"></div>
+    <div class="spinner-grow text-secondary" style="width: 3rem; height: 3rem;"></div>
+    <div class="spinner-grow text-dark" style="width: 3rem; height: 3rem;"></div>
     <!--
         <br/>
         <span>{{ trans('messages.processingpayment') }}</span>
