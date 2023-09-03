8c89d115f770f1511ea3afd0af06d35d24fa1855
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-09-02 22:50:02 -0400

    update

--- resources/views/pdf/boardingPass.blade.php
+++ resources/views/pdf/boardingPass.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap');
+		    font-family: 'Noto Sans Thai', sans-serif;
         </style>
     </head>
     <body>
