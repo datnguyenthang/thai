7d58157b1085779e8375b135bfe87319ad481741
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-11-02 22:09:22 -0400

    update

--- app/Http/Livewire/Backend/Manager/ManagerProcessOrder.php
+++ app/Http/Livewire/Backend/Manager/ManagerProcessOrder.php
@@ -18,7 +18,7 @@ class ManagerProcessOrder extends Component
 
     public function render()
     {
-        return view('livewire.backend.admin.admin-process-order')
-                    ->layout('admin.layouts.app');
+        return view('livewire.backend.manager.manager-process-order')
+                    ->layout('manager.layouts.app');
     }
 }
