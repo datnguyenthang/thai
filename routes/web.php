cff5af944813f3b1141c6d4d1451795b4242fbd8
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-11-13 23:00:25 -0500

    update

--- routes/web.php
+++ routes/web.php
@@ -93,7 +93,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/yearlyreport', App\Http\Livewire\Component\Report\Yearly::class)->name('yearlyReport');
     Route::get('/saleperformance', App\Http\Livewire\Component\Report\Performance::class)->name('salePerformance');
     Route::get('/cashflow', App\Http\Livewire\Component\Report\Cashflow::class)->name('cashflow');
-    Route::get('/debt', App\Http\Livewire\Component\Report\debt::class)->name('debt');
+    Route::get('/debt', App\Http\Livewire\Component\Report\Debt::class)->name('debt');
     /*------All Manager Routes List------*/
     Route::middleware(['user-access:manager'])->group(function () {
 
