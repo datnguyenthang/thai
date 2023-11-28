69e18a501f2a057e93072360b2af57fe94098fb8
datnguyenthang <nguyen.thang.dat.nt@gmail.com>
2023-11-27 19:36:17 -0500

    update

--- app/Exports/RidesExport.php
+++ app/Exports/RidesExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class RidesExport implements FromCollection, WithHeadings
+class RidesExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -41,10 +42,42 @@ class RidesExport implements FromCollection, WithHeadings
             'Pickup',
             'Dropoff',
             'Full Name',
+            'Agent Name',
             'Customer Type',
             'Booking User',
             'Ticket Type',
             'Status',
+            'Payment method',
+            'Payment status'
+        ];
+    }
+
+    public function map($row): array
+    {
+        return [
+            $row->id,
+            $row->ridename,
+            $row->fromLocationName,
+            $row->toLocationName,
+            $row->departTime,
+            $row->returnTime,
+            $row->departDate,
+            $row->code,
+            $row->phone,
+            $row->email,
+            $row->adultQuantity,
+            $row->childrenQuantity,
+            $row->price,
+            $row->pickup,
+            $row->dropoff,
+            $row->fullname,
+            $row->agentName,
+            $row->customerType,
+            $row->name,
+            $row->ticket,
+            $row->status,
+            $row->paymentMethod,
+            PAYMENTSTATUS[$row->paymentStatus],
         ];
     }
 }
--- app/Lib/DashboardLib.php
+++ app/Lib/DashboardLib.php
@@ -88,13 +88,14 @@ class DashboardLib {
     }
 
     public static function exportRides($rideId = 0, $fromDate = 0, $toDate = 0, $depart = 0, $dest = 0) {
-        $passengers = Ride::select('rides.id', 'rides.name as Ridename', 'fl.name as fromLocationName', 'tl.name as toLocationName', 'rides.departTime', 'rides.returnTime', 'rides.departDate',
+        $passengers = Ride::select('rides.id', 'rides.name as ridename', 'fl.name as fromLocationName', 'tl.name as toLocationName', 'rides.departTime', 'rides.returnTime', 'rides.departDate',
                                 'o.code', 'o.code', 'o.phone', 'o.email', 'o.adultQuantity', 'o.childrenQuantity', 'ot.price', 'ot.pickup', 'ot.dropoff',
-                                DB::raw('CONCAT(COALESCE(o.firstname, ""), " ", COALESCE(o.lastName, "")) as fullname'),
-                                DB::raw('CASE WHEN o.customerType <> 0 THEN ct.name ELSE "Online" END AS CustomerType'), 'u.name',
-                                DB::raw('CASE WHEN ot.type = '.ONEWAY.' THEN "Departure" ELSE "Return" END AS Ticket'),
-                                DB::raw('CASE WHEN os.status = '.CONFIRMEDORDER.' THEN "Confirm" ELSE "Not Confirm" END AS Status')
-                                )
+                                DB::raw('CONCAT(COALESCE(o.firstname, ""), " ", COALESCE(o.lastName, "")) as fullname'),'a.name as agentName',
+                                DB::raw('CASE WHEN o.customerType <> 0 THEN ct.name ELSE "Online" END AS customerType'), 'u.name',
+                                DB::raw('CASE WHEN ot.type = '.ONEWAY.' THEN "Departure" ELSE "Return" END AS ticket'),
+                                DB::raw('CASE WHEN os.status = '.CONFIRMEDORDER.' THEN "Confirm" ELSE "Not Confirm" END AS status'),
+                                'pm.name as paymentMethod', 'op.paymentStatus'
+                            )
                     ->leftJoin('locations as fl', 'rides.fromLocation', '=', 'fl.id')
                     ->leftJoin('locations as tl', 'rides.toLocation', '=', 'tl.id')
                     ->join('order_tickets as ot' , function ($otjoin) {
@@ -107,6 +108,12 @@ class DashboardLib {
                         $join->on('o.id', '=', 'os.orderId')
                             ->whereRaw('os.id = (select max(id) from order_statuses where order_statuses.orderId = o.id)');
                     })
+                    ->leftJoin('order_payments as op', function($opjoin) {
+                        $opjoin->on('o.id', '=', 'os.orderId')
+                            ->whereRaw('op.id = (select max(id) from order_payments where order_payments.orderId = o.id)');
+                    })
+                    ->leftJoin('agents as a', 'a.id', '=', 'o.agentId')
+                    ->leftJoin('payment_methods as pm', 'pm.id', '=', 'op.paymentMethod')
                     ->leftJoin('customer_types as ct', 'ct.id', '=', 'o.customerType')
                     ->leftJoin('users as u', 'u.id', '=', 'o.userId')
                     ->where(function ($query) use ($rideId, $fromDate, $toDate, $depart, $dest) {
